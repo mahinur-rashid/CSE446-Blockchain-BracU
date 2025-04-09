b50a05c0b16a927074a4302383ea43d00c32badb
Mahinur Rashid <mahinurrashid27@gmail.com>
2025-04-09 19:45:37 +0600

    fix

--- Lab03/checkpoint1.sol
+++ Lab03/checkpoint1.sol
@@ -1,13 +1,10 @@
 /// SPDX-License-Identifier: MIT
-
-
-
 pragma solidity >= 0.8.2 <0.9.0;
 
 
 contract Checkpoint1 {
     uint256 studentID = 22301714;
-    string name = "Mahinur";
+    string name = "Mahinur Raahid";
     
     function StudentName(uint256 _studentID) public view returns(string memory) {
         if(_studentID == studentID) {
