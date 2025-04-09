f7b5a6d2ecafa53cd58fc6c0902a0386497e7dd9
Mahinur Rashid <mahinurrashid27@gmail.com>
2025-04-09 19:42:13 +0600

    Create checkpoint1a.sol

---
+++ Lab03/checkpoint1a.sol
@@ -0,0 +1,17 @@
+/// SPDX-License-Identifier: MIT
+
+
+pragma solidity >= 0.8.2 <0.9.0;
+
+contract Checkpoint1 {
+    string private studentID = "22222222";
+    string private name = "Mahinur";
+
+    function StudentName(string memory _studentID) public view returns (string memory) {
+        if (keccak256(abi.encodePacked(_studentID)) == keccak256(abi.encodePacked(studentID))) {
+            return name;
+        } else {
+            return "This is not your ID";
+        }
+    }
+}
